8f7ae1ab39ce43e224b7f711bd4da66fa4fc82ca
Tim4ukys <tim4ukys.dev@yandex.ru>
2023-06-14 11:18:10 +0700

    Добавьте файлы проекта.

---
+++ AsiPluginTemplate/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "main.hpp"
+
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    if (fdwReason == DLL_PROCESS_ATTACH) {
+        DisableThreadLibraryCalls(hinstDLL);
+
+    }
+    else if (fdwReason == DLL_PROCESS_DETACH) {
+
+    }
+    return TRUE;
+}
