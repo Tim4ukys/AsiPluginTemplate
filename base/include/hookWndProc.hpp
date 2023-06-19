6f38e67ae7e7293a7afe88695b8aca8357b70ef1
Tim4ukys <tim4ukys.dev@yandex.ru>
2023-06-19 13:34:18 +0700

    add hook on WndProc

---
+++ base/include/hookWndProc.hpp
@@ -0,0 +1,38 @@
+/*
+    AsiPluginTemplate header file
+    Authors: tim4ukys
+    url: https://github.com/Tim4ukys/AsiPluginTemplate
+    tg: https://t.me/tim4ukys
+    Do not delete this comment block. Respect others' work!
+*/
+#ifndef __BASE_WND_PROC_HPP__
+#define __BASE_WND_PROC_HPP__
+
+#include <boost/signals2.hpp>
+#include <cyanide/hook_impl_polyhook.hpp>
+
+namespace base {
+    namespace hooks {
+
+        class hkWndProc {
+        public:
+            using wndproc_t = HRESULT(__stdcall*)(HWND, UINT, WPARAM, LPARAM);
+            using wndproc_hk_t = std::function<HRESULT(wndproc_t, HWND, UINT, WPARAM, LPARAM)>;
+
+        protected:
+            std::unique_ptr<cyanide::polyhook_x86<wndproc_t, wndproc_hk_t>> m_wndProc;
+        };
+
+        class hkWndProcSAMP : public hkWndProc {
+        public:
+            explicit hkWndProcSAMP(wndproc_hk_t proc);
+        };
+
+        class hkWndProcGame : public hkWndProc {
+            explicit hkWndProcGame(wndproc_hk_t proc);
+        };
+
+    }
+}
+
+#endif
---
+++ base/src/hookWndProc.cpp
@@ -0,0 +1,40 @@
+/*
+    AsiPluginTemplate source file
+    Authors: tim4ukys
+    url: https://github.com/Tim4ukys/AsiPluginTemplate
+    tg: https://t.me/tim4ukys
+    Do not delete this comment block. Respect others' work!
+*/
+#include "hookWndProc.hpp"
+#include <Windows.h>
+#include "SAMP.hpp"
+#include "base_safe_check.hpp"
+
+namespace base {
+    namespace hooks {
+        hkWndProcSAMP::hkWndProcSAMP(wndproc_hk_t proc) {
+            const DWORD OFFSETS[6]{
+                {0x5DB40}, // 037-r1
+                {0x5DC10}, // 037-r2
+                {0x60EE0}, // 037-r3_1
+                {0x61610}, // 037-r4
+                {0x61650}, // 037-r4_2
+                {0x61650}, // 037-r5_1
+            };
+            BASE_CHECK_ARRAYSIZE(OFFSETS, SAMP::SAMP_COUNT_SUPPORT_VERSIONS);
+            const auto& offset = OFFSETS[static_cast<int>(SAMP::getRef()->getSAMPVersion())];
+
+            m_wndProc = std::make_unique<typename decltype(m_wndProc)::element_type>(
+                std::move(SAMP::getRef()->getDLL().getAddr<wndproc_t>(offset)),
+                std::move(proc));     
+            m_wndProc->install();
+        }
+
+        hkWndProcGame::hkWndProcGame(wndproc_hk_t proc) {
+            m_wndProc = std::make_unique<typename decltype(m_wndProc)::element_type>(
+                std::move(reinterpret_cast<wndproc_t>(0x747EB0)),
+                std::move(proc));
+            m_wndProc->install();
+        }
+    }
+}
